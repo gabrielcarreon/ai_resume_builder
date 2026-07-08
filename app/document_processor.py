338649bf787bdda3f0cad8cdaf5d33f7197849ae
gabriel carreon <gabrielcarreon02@gmail.com>
2026-07-08 14:01:34 +0800

    feat: document_processing

---
+++ app/document_processor.py
@@ -0,0 +1,8 @@
+# from llama_index import SimpleDirectoryReader
+
+
+def main(file):
+    return file
+
+if __name__ == "__main__":
+    main()
--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
 import gradio as gr
+import document_processor as dp
 
-def main(text: str):
-    return f"Hello, {text}!reload verifiedzdddcvvv"
+def main(resume: gr.File, skills: str):
+    dp(resume.name)
+    return resume
 
 
 interface = gr.Interface(
     fn=main,
-    inputs=[gr.File(label="Resume")],
+    inputs=[gr.File(label="Resume"), gr.File(label="Skills MD")],
     outputs=[gr.File(label="Translated Resume")],
     title="Hello World App",
 )
 
 if __name__ == "__main__":
-    interface.launch(server_name="127.0.0.1", server_port=8000)
+    interface.launch(server_name="0.0.0.0", server_port=8000)
